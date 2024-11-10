d0ed8778e8b4cc8f4a12be10220e73f693903811
Lazizbekkk <mamadiyorovlazizbek97@gamil.com>
2024-11-10 19:30:31 +0500

    google bilan kirish qismini qildim firebasega saqlamaydi MVVM siz hozircha

--- app/src/main/java/com/example/mamadiyorov_lazizbek/chatappgita/repository/AppRepositoryImpl.kt
+++ app/src/main/java/com/example/mamadiyorov_lazizbek/chatappgita/repository/AppRepositoryImpl.kt
@@ -1,21 +1,30 @@
 package com.example.mamadiyorov_lazizbek.chatappgita.repository
 
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
 class AppRepositoryImpl: AppRepository {
-    private val auth = FirebaseAuth.getInstance()
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()  // FirebaseAuth obyektini yaratish
 
-    // Google bilan kirish
     override fun firebaseAuthWithGoogle(idToken: String, onUserAuthenticated: (() -> Unit)?, onAuthFailed: ((String) -> Unit)?) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                   onUserAuthenticated?.invoke()
-                } else {
-                    onAuthFailed?.invoke("Sizda kuzda tutilmagan xatolik yuz berdi")
+        if (idToken.isNotEmpty()) {
+            Log.d("KKKKKK", "Kelgan token: $idToken")
+            val credential = GoogleAuthProvider.getCredential(idToken, null)
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Agar autentifikatsiya muvaffaqiyatli bo'lsa, userni autentifikatsiya qilishni kutib turing
+                        onUserAuthenticated?.invoke()
+                    } else {
+                        // Agar autentifikatsiya muvaffaqiyatli bo'lmasa, xatolikni ko'rsating
+                        onAuthFailed?.invoke("Sizda kuzda tutilmagan xatolik yuz berdi")
+                    }
                 }
-            }
+        } else {
+            Log.d("KKKKKK", "Kelgan token: $idToken")
+            // Agar idToken bo'sh bo'lsa, xatolik qaytarish
+            onAuthFailed?.invoke("Token topilmadi")
+        }
     }
 }
