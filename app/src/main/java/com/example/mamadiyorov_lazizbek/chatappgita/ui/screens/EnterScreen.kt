cb8ac350e748b44a31bacb9cd4f949f23d10e531
Lazizbekkk <mamadiyorovlazizbek97@gamil.com>
2024-11-10 15:33:49 +0500

    enter screen uzgartirildi item qilindi keyingi ishim logikasini qilish Lazizbek

---
+++ app/src/main/java/com/example/mamadiyorov_lazizbek/chatappgita/ui/screens/EnterScreen.kt
@@ -0,0 +1,8 @@
+package com.example.mamadiyorov_lazizbek.chatappgita.ui.screens
+
+import androidx.fragment.app.Fragment
+import com.example.mamadiyorov_lazizbek.chatappgita.R
+
+class EnterScreen: Fragment(R.layout.screen_enter) {
+
+}
